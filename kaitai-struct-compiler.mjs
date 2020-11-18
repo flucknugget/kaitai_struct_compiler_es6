3ef783d935d7143c0b882e2573015f6f92cd10ca
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-18 22:03:10 +0000

    Deploy flucknugget/kaitai_struct_compiler_es6 to github.com/flucknugget/kaitai_struct_compiler_es6.git:master

--- kaitai-struct-compiler.mjs
+++ kaitai-struct-compiler.mjs
@@ -1,4 +1,4 @@
-/* kaitai_struct_compiler 0.9-SNAPSHOT20201101.191752.58782311 */
+/* kaitai_struct_compiler 0.9-SNAPSHOT20201118.213751.79813f60 */
 
 'use strict';
 /* Scala.js runtime support
@@ -3888,9 +3888,9 @@ class $c_Lio_kaitai_struct_Version$ extends $c_O {
   };
   init___() {
     this.name$1 = "kaitai-struct-compiler-js";
-    this.version$1 = "0.9-SNAPSHOT20201101.191752.58782311";
-    this.gitCommit$1 = "58782311";
-    this.gitTime$1 = "2020-11-01T19:17:52+00:00";
+    this.version$1 = "0.9-SNAPSHOT20201118.213751.79813f60";
+    this.gitCommit$1 = "79813f60";
+    this.gitTime$1 = "2020-11-18T21:37:51+00:00";
     return this
   };
 }
